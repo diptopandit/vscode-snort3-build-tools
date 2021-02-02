d751740f5f95b2a65b43e473086a1013931de64a
Dipto Pandit (dipandit) <dipandit@cisco.com>
2021-02-02 18:07:16 +0530

    Fixed duplicate terminal problem

--- src/build_tools.ts
+++ src/build_tools.ts
@@ -25,6 +25,7 @@ class snort3BuildToolsTerminal
     private writer:vscode.EventEmitter<string>;
     private static term_count:number=0;
     private term_id:number;
+    private wip:boolean = false;
     constructor( private readonly task:string, private readonly statusItem:vscode.StatusBarItem,
         private readonly status_text:string, private readonly cmd:string, private readonly args:string[],
         private readonly options:child_process.SpawnOptions, private readonly parent:any)
@@ -32,17 +33,19 @@ class snort3BuildToolsTerminal
         this.writer = new vscode.EventEmitter<string>();
         this.onDidWrite = this.writer.event;
         snort3BuildToolsTerminal.term_count++;
-        this.term_id = snort3BuildToolsTerminal.term_count
+        this.term_id = snort3BuildToolsTerminal.term_count;
     }
     get_term_id():number { return this.term_id;}
     async open() {
+        this.wip = true;
         this.writer.fire('*** Starting '+this.task+' snort3 task ***\r\n');
         this.statusItem.text=`$(`+this.status_text+`~spin)`;
         this.cp = child_process.spawn(this.cmd,this.args, this.options);
         if(!this.cp || !this.cp.pid) {
             this.writer.fire('**** ERROR starting '+this.task+' task ****\r\n');
-            this.statusItem.text=this.status_text;
+            this.statusItem.text=`$(`+this.status_text+`)`;
             this.parent.busy = false;
+            this.wip = false;
         }
         this.cp!.stdout!.setEncoding('utf8');
         this.cp!.stderr!.setEncoding('utf8');
@@ -57,6 +60,7 @@ class snort3BuildToolsTerminal
             this.cp = undefined;
             this.statusItem.text=`$(`+this.status_text+`)`;
             this.parent.busy = false;
+            this.wip = false;
         });
     }
 
@@ -65,12 +69,10 @@ class snort3BuildToolsTerminal
             this.cp.kill("SIGKILL");
             this.cp = undefined;
         }
-        this.statusItem.text=`$(`+this.status_text+`)`;
-        this.parent.busy = false;
     }
 
     async handleInput() {
-        if(this.term_id)
+        if(this.term_id && !this.wip)
             this.parent.dispose_terminal(this.term_id);
     }
 }
